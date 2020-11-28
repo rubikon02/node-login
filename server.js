193c923a8c0ad9a5f9da1be14f3b97d2f4c04e07
rubikon02 <rubikon02@gmail.com>
2020-11-28 18:21:39 +0100

    kolejna zmiana w pliku servera

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express()
 var PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 
 app.get("/", function (req, res) {
-    res.send("<h1>first app on heroku - mała zmiana w pliku</h1>")
+    res.send("<h1>first app on heroku - kolejna mała zmiana w pliku</h1>")
 })
 
 app.listen(PORT, function () {
